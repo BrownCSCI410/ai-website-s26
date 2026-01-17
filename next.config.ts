7cc8c26267160939a434bbf06b89ca70598fb436
John Lockwood <johnlockwood307@gmail.com>
2026-01-17 14:51:48 -0500

    changed basePath in config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
+    basePath: '/ai-website-s26'
 };
 
 export default nextConfig;
