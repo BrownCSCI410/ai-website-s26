38d2f71044756613b0ce3e67a31c7ede8b22e39f
Sarah Liao <sarah@Sarahs-MacBook-Air-2.local>
2026-03-24 21:56:23 -0400

    adding src 3, hw 5+6

--- src/app/components/Discussion.tsx
+++ src/app/components/Discussion.tsx
@@ -13,12 +13,13 @@ const DISCUSSION_DATA = [
     { topic: "HW 1 and 2", date: "2/9 - 2/13", link: "https://docs.google.com/presentation/d/1jqIHQsJzKm6iC-IIIoVKTtuonx-5bslh5YlRunVlQw8/edit?usp=sharing", quiz: "https://docs.google.com/document/d/1WuEmw4uMvogcjiS7eStASslrZG89-N3Ev9wgWhXiIqI/edit?usp=sharing"},
     { topic: "Bias in Data", date: "2/23 - 2/27", link: "https://docs.google.com/presentation/d/1JWXv3U4M9M0Pnt290wR1TgHJJZyNULypHTbNho0gEdk/edit?usp=sharing" },
     { topic: "HW 3 and 4", date: "3/2 - 3/6", link: "https://docs.google.com/presentation/d/1lce1FgabbKskdzPpApQZkwJXmPbecRannUg2m9WPo8k/edit?usp=sharing" , quiz: "https://docs.google.com/document/d/1sRfMfUYfluL9zbi4VdJcxBfoa-1GKjkoH2j8vrjR1Wg/edit?usp=sharing"},
-    { topic: "SRC - TBD", date: "3/9 - 3/13", link: "https://www.youtube.com/watch?v=4Jdp_dkduJc" },
-    { topic: "HW 5 and 6", date: "3/16 - 3/20", link: "https://www.youtube.com/watch?v=TJryIc0Cwy4", quiz: "#" },
+    { topic: "Automated Decision-Making", date: "3/9 - 3/13", link: "https://docs.google.com/presentation/d/1N3tyXpi5mwsPr5ZaCB40VKReWrTOHbeXve002kLxmaE/edit?usp=sharing" },
+    { topic: "HW 5 and 6", date: "3/16 - 3/20", link: "https://docs.google.com/presentation/d/1Qmm8IgYsUuKhrSd16298VFsTj6owaSVIq_hmE-KF-Ws/edit?usp=sharing", quiz: "https://docs.google.com/document/d/1SMe8m3d4lzRJRtLNfJs4woY5VcgbdBsZXcXaMEoErc0/edit?usp=sharing" },
     { topic: "SRC - TBD", date: "3/30 - 4/3", link: "https://www.youtube.com/watch?v=vv8MI_y1Lsg" },
     { topic: "SRC - TBD", date: "4/6 - 4/10", link: "https://www.youtube.com/watch?v=XH3Xu1-cvII" },
     { topic: "HW 7, 8 and 9", date: "4/13 - 4/17", link: "https://www.youtube.com/watch?v=kqhtEpXoOwcI", quiz: "#" },
 ];
+
 
 function DiscussionRow({ topic, date, slidesLink, quiz }: DiscussionProps) {
     return (
